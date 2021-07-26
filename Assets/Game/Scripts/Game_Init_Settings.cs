daed2cd1cb41db4fb9031d5e1750e4ef02aa69d2
Richard Alejandro Nuñez Vela <richarpro1@gmail.com>
2021-07-26 03:20:13 -0400

    New stuff

---
+++ Assets/Game/Scripts/Game_Init_Settings.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Init_Settings : MonoBehaviour
+{
+    void Awake()
+    {
+        Physics.autoSimulation = false;
+        Application.targetFrameRate = 30;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Game/Scripts/ObjectActions.cs
+++ Assets/Game/Scripts/ObjectActions.cs
@@ -2,28 +2,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class ObjectActions : MonoBehaviour
 {
     public GameObject SelectedGameOBJ;
+    private GameObject ObjCache;
 
     [SerializeField] private Slider xRotateSliderOBJ;
     [SerializeField] private Slider yRotateSliderOBJ;
+    [SerializeField] private Slider zRotateSliderOBJ;
 
     private float xRotation;
     private float yRotation;
+    private float zRotation;
     private Vector3 vRotation;
 
-void Awake()
-{
-    SelectedGameOBJ = GameObject.Find("Sphere");
-}
-    public void SliderRotate()
-    {
-        xRotation = xRotateSliderOBJ.value;
-        yRotation = yRotateSliderOBJ.value;
-        vRotation = new Vector3(xRotation, yRotation, 0);
+    [SerializeField] private float speed;
 
-        SelectedGameOBJ.transform.eulerAngles = vRotation;
+    void Awake()
+    {
+        SelectedGameOBJ = GameObject.Find("Sphere");
+
+    }
+    void Update()
+    {
+        UnableFloorSliderRotation();
+
+        SliderResetChecker();
+
+    }
+
+    private void SliderResetChecker()
+    {
+        if (xRotateSliderOBJ.value > 0)
+        {
+            SelectedGameOBJ.transform.Rotate((Vector3.right * Time.deltaTime) * (xRotateSliderOBJ.value * speed));
+
+        }
+
+        if (xRotateSliderOBJ.value < 0)
+        {
+            SelectedGameOBJ.transform.Rotate((Vector3.left * Time.deltaTime) * (xRotateSliderOBJ.value * speed) * -1);
+
+        }
+
+        if (yRotateSliderOBJ.value > 0)
+        {
+            SelectedGameOBJ.transform.Rotate((Vector3.up * Time.deltaTime) * (yRotateSliderOBJ.value * speed));
+        }
+
+        if (yRotateSliderOBJ.value < 0)
+        {
+            SelectedGameOBJ.transform.Rotate((Vector3.down * Time.deltaTime) * (yRotateSliderOBJ.value * speed) * -1);
+        }
+
+        if (zRotateSliderOBJ.value > 0)
+        {
+            SelectedGameOBJ.transform.Rotate((Vector3.forward * Time.deltaTime) * (zRotateSliderOBJ.value * speed));
+        }
+
+        if (zRotateSliderOBJ.value < 0)
+        {
+            SelectedGameOBJ.transform.Rotate((Vector3.back * Time.deltaTime) * (zRotateSliderOBJ.value * speed) * -1);
+        }
+    }
+
+    void UnableFloorSliderRotation()
+    {
+        if (SelectedGameOBJ.name == "Floor")
+        {
+
+            xRotateSliderOBJ.interactable = false;
+            yRotateSliderOBJ.interactable = false;
+            zRotateSliderOBJ.interactable = false;
+        }
+        else
+        {
+            xRotateSliderOBJ.interactable = true;
+            yRotateSliderOBJ.interactable = true;
+            zRotateSliderOBJ.interactable = true;
+        }
+    }
+
+    public void ResetSliderValue()
+    {
+        xRotateSliderOBJ.value = 0;
+        yRotateSliderOBJ.value = 0;
+        zRotateSliderOBJ.value = 0;
     }
 }
